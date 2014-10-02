ba11a7ff9290f111ba77b58e675ec4a66cd74bb2
Mariia <zryaninam@gmail.com>
2014-10-02 14:20:57 +0200

    add SnapshotData.java and SnaphotWriter.java (have to be tested)

---
+++ src/no_de/inf5090/visualizingsensordata/persistency/SnapshotWriter.java
@@ -0,0 +1,36 @@
+package no_de.inf5090.visualizingsensordata.persistency;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import no_de.inf5090.visualizingsensordata.domain.SnapshotData;
+
+import android.graphics.Bitmap.CompressFormat;
+
+public class SnapshotWriter {
+	public SnapshotWriter() {
+		
+	}
+	public void writeBitmap(String path, SnapshotData snapshotData) {
+		try {
+			FileOutputStream fos = new FileOutputStream(path + "/" + snapshotData.getName() + ".jpg");
+			snapshotData.getBitmap().compress(CompressFormat.JPEG, 75, fos);
+		    try {
+				fos.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    try {
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		catch (FileNotFoundException  e) {
+		}
+	}
+
+}
