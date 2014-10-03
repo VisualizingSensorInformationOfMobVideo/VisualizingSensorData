9709d6311372aceada8b14684b00772dc4873455
Henrik Steen <henrist@henrist.net>
2014-10-03 07:29:29 +0200

    Can now store float-arrays in SensorData. Should probably be done in a better way..

--- src/no_de/inf5090/visualizingsensordata/domain/SensorData.java
+++ src/no_de/inf5090/visualizingsensordata/domain/SensorData.java
@@ -4,10 +4,20 @@ import java.util.Date;
 
 public class SensorData {
 	private Object sensor;
-	private float value;
+	private float[] values;
 	private Date timestamp;
 	
 	public SensorData() { };
+	public SensorData(Object sensor, float value) {
+		setSensor(sensor);
+		setValue(value);
+		setTimestamp(new Date());
+	}
+	public SensorData(Object sensor, float[] values) {
+		setSensor(sensor);
+		setValues(values);
+		setTimestamp(new Date());
+	}
 	public SensorData(Object sensor, float value, Date timestamp) {
 		setSensor(sensor);
 		setValue(value);
@@ -21,10 +31,16 @@ public class SensorData {
 		this.sensor = sensor;
 	}
 	public float getValue() {
-		return value;
+		return values[0];
+	}
+	public float[] getValues() {
+		return values;
 	}
 	public void setValue(float value) {
-		this.value = value;
+		this.values = new float[]{value};
+	}
+	public void setValues(float[] values) {
+		this.values = values;
 	}
 	public Date getTimestamp() {
 		return timestamp;
