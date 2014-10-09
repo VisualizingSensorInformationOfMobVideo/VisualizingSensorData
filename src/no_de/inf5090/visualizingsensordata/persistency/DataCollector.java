ef64943fb38d674a6a9188f21c10917422e6474c
Henrik Steen <henrist@henrist.net>
2014-10-09 22:48:28 +0200

    Refactored generation of XML.

--- src/no_de/inf5090/visualizingsensordata/persistency/DataCollector.java
+++ src/no_de/inf5090/visualizingsensordata/persistency/DataCollector.java
@@ -1,10 +1,16 @@
 package no_de.inf5090.visualizingsensordata.persistency;
 
+import android.util.Log;
 import no_de.inf5090.visualizingsensordata.application.Utils;
 import no_de.inf5090.visualizingsensordata.domain.AbstractLogicalSensorData;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.*;
 
 /**
@@ -72,6 +78,45 @@ public abstract class DataCollector implements Observer {
         }
 
         return doc;
+    }
+
+    /**
+     * Fill a Writer-object with transformed XML data and empty the data list
+     */
+    protected void transformXml(Writer writer) {
+        Document doc = generateXmlDom();
+        emptyData();
+
+        DOMSource source = new DOMSource(doc);
+
+        // Set up transformer
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            Properties outFormat = new Properties();
+            outFormat.setProperty(OutputKeys.INDENT, "yes");
+            outFormat.setProperty(OutputKeys.METHOD, "xml");
+            outFormat.setProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            outFormat.setProperty(OutputKeys.VERSION, "1.0");
+            outFormat.setProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperties(outFormat);
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+            transformer.transform(source, new StreamResult(writer));
+        //} catch (TransformerConfigurationException e) {
+        } catch (TransformerException e) {
+            Log.e("DataCollector", "TransformerException detected!");
+        }
+    }
+
+    /**
+     * Generate XML data as a string and empty the data list
+     */
+    protected String getXmlString() {
+        StringWriter writer = new StringWriter();
+        transformXml(writer);
+        return writer.getBuffer().toString();
     }
 
     /**
