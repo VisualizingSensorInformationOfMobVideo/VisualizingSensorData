36ae74cf50850003830147cbe9a0f6635f7bf220
mikaello <mikaello@ulrik>
2014-10-09 17:15:52 +0200

    Fixed some bugs

--- src/no_de/inf5090/visualizingsensordata/transmission/XmlTransmission.java
+++ src/no_de/inf5090/visualizingsensordata/transmission/XmlTransmission.java
@@ -1,34 +1,41 @@
-package src.no_de.inf5090.visualizingsensordata.transmission;
+package no_de.inf5090.visualizingsensordata.transmission;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.HTTP;
 
+/**
+ * Class for sending XML
+ */
 public class XmlTransmission {
 
-
-
-public XmlTransmission(){
-	//xml Sending
-		public void dataSend(String fpath, File file){
-			DefaultHttpClient httpClient = new DefaultHttpClient();
+		public void dataSend(String fpath, File file) {
+			//DefaultHttpClient httpClient = new DefaultHttpClient();
 			HttpPost httppost = new HttpPost("134.155.92.184");
 			String filePath = this.getFilesDir().getAbsolutePath();
 			File f=new File(filePath,"file.xml");
 			//byte[] data = FileOperator.readBytesFromFile(f);
 			String content=getFileContent(file);
-			StringEntity se = new StringEntity(content, HTTP.UTF_8);
+			StringEntity se = null;
+			try {
+				se = new StringEntity(content, HTTP.UTF_8);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
 			se.setContentType("text/xml");
 			httppost.setEntity(se);
 			f.delete();
 		}
+		
 		private File getFilesDir() {
 			// TODO Auto-generated method stub
 			return null;
 		}
+		
 		private String getFileContent(File file) {
 			// TODO Auto-generated method stub
 			return null;
