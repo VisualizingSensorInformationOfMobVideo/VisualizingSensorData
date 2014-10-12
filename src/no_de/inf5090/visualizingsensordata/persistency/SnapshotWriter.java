ef849d73bac2948345ff551be1a24553e3de457a
Mariia <zryaninam@gmail.com>
2014-10-12 23:25:09 +0200

    back to JPEG compression

--- src/no_de/inf5090/visualizingsensordata/persistency/SnapshotWriter.java
+++ src/no_de/inf5090/visualizingsensordata/persistency/SnapshotWriter.java
@@ -1,17 +1,24 @@
 package no_de.inf5090.visualizingsensordata.persistency;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Observer;
 import java.util.zip.GZIPOutputStream;
 
+import no_de.inf5090.visualizingsensordata.userInterface.VideoCapture;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Base64;
 import android.util.Log;
 
 public class SnapshotWriter extends AsyncTask<byte[], Void, Void>{
 	private Observer snapshotObserver; 
-    // private int jpegQuality = 100;
+	private int jpegQuality = 50;
+	//private int photoWidth = 320;
+	//private int photoHeight = 240;
     /**
      * Constructor: initiates snapshotwriter
      */
@@ -21,7 +28,7 @@ public class SnapshotWriter extends AsyncTask<byte[], Void, Void>{
 
     @Override
     protected Void doInBackground(byte[]... data) {
-        try {
+        /*try {
         	ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         	try{
                  GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream);
@@ -29,31 +36,35 @@ public class SnapshotWriter extends AsyncTask<byte[], Void, Void>{
                  gzipOutputStream.close();
              } catch(IOException e){
                  throw new RuntimeException(e);
-             }
+             }*/
              // System.out.printf("Compression ratio %f\n", (1.0f * data.length/byteArrayOutputStream.size()));
              
-        	// String encodedImage = Base64.encodeToString(data[0], Base64.DEFAULT);
-             String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
-        	 snapshotObserver.update(null, encodedImage);
+         	//String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+    	    // String encodedImage = Base64.encodeToString(data[0], Base64.DEFAULT);
         	
-        	/*
+        	
+        	
         	Bitmap image = BitmapFactory.decodeByteArray(data[0], 0, data[0].length);
-            String fPath = VideoCapture.appDir.getPath() + "/" + System.currentTimeMillis() + ".jpg";
-            FileOutputStream fos = new FileOutputStream(fPath);
+        	//image.createScaledBitmap(image, PHOTO_WIDTH, PHOTO_HEIGHT, true);
+            //String fPath = VideoCapture.appDir.getPath() + "/" + System.currentTimeMillis() + ".jpg";
+           // FileOutputStream fos = new FileOutputStream(fPath);
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             image.compress(Bitmap.CompressFormat.JPEG, jpegQuality, out);
-          //  fos.write(data[0]);
-            fos.write(out.toByteArray());
+          /*fos.write(data[0]);
+           fos.write(out.toByteArray());
             fos.flush();
             fos.close();
+            
+            snapshotData.add(fPath); // add full path of the just taken snapshot to the queue*/
+            String encodedImage = Base64.encodeToString(out.toByteArray(), Base64.DEFAULT);
+        	snapshotObserver.update(null, encodedImage);
+        	
 
-            snapshotData.add(fPath); // add full path of the just taken snapshot to the queue
-*/
             //Log.d("snap", "onPictureTaken - wrote bytes: " + data.length + " to " + fPath);
             Log.d("snap", "onPictureTaken - wrote bytes: " + data.length + " to SnapshotObserver");
-        } catch (Exception e) {
+        /*} catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
 
         return null;
     }
